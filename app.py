911cfbea4c0327f1b4663b67671a715ddd4b7413
Kamil Pawlowski <20102337@mail.wit.ie>
2025-08-29 15:08:23 +0100

    Implement error handling for creating interfaces

--- app.py
+++ app.py
@@ -10,21 +10,19 @@ from msrest.authentication import ApiKeyCredentials
 from hardware_manager import HardwareManager
 
 
+# Interface creation attempts limit
+MAX_INTERFACE_ATTEMPTS = 3
+
 # Authentication
 BLYNK_AUTH = "7os_iwPxKnhgay7WX-KhXgzXi9mgb2Ib"
 AZURE_CONTAINER_AUTH = "DefaultEndpointsProtocol=https;AccountName=smskman1510;AccountKey=6uIe1RPi+CLyvKpJHIGExkPAse9pRepXThBE+tIkobiPawHuPIQS5NvoiT/ycuJP8Uw9HGn3o8+3+AStJoSimQ==;EndpointSuffix=core.windows.net"
 AZURE_CONTAINER_NAME = "plant-monitoring-images"
 CUSTOM_VISION_URL = 'https://northeurope.api.cognitive.microsoft.com/customvision/v3.0/Prediction/675f1359-659d-416c-a23f-134e3c3ca9ba/classify/iterations/Iteration2/image'
 CUSTOM_VISION_KEY = 'd09e97f6ea7f48f3b7b9128c9fc5bc58'
-
-# TODO Move this into a function in a try except block at the start
-parts = CUSTOM_VISION_URL.split('/')
-endpoint = 'https://' + parts[2]
-project_id = parts[6]
-iteration_name = parts[9]
-
-prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": CUSTOM_VISION_KEY})
-custom_vision = CustomVisionPredictionClient(endpoint, prediction_credentials)
+CUSTOM_VISION_URL_PARTS = CUSTOM_VISION_URL.split('/')
+CUSTOM_VISION_ENDPOINT = 'https://' + CUSTOM_VISION_URL_PARTS[2]
+CUSTOM_VISION_PROJECT_ID = CUSTOM_VISION_URL_PARTS[6]
+CUSTOM_VISION_ITERATION_NAME = CUSTOM_VISION_URL_PARTS[9]
 
 # Delays (seconds)
 MOTION_CHECK_DELAY = 0.1 # Between motion checks
@@ -38,12 +36,10 @@ MISTING_FREQUENCY = 2 # Days between misting (every xth day)
 
 
 # Hardware and cloud interfaces
-hardware_manager = HardwareManager()
-blynk = BlynkLib.Blynk(BLYNK_AUTH) # Initialise the Blynk instance
-container_client = ContainerClient.from_connection_string(
-    AZURE_CONTAINER_AUTH,
-    AZURE_CONTAINER_NAME
-) # Azure blob storage container
+hardware_manager = None
+blynk = None
+container = None # Azure blob storage container client
+custom_vision = None
 
 # User config
 daily_task_hour = 12
@@ -57,6 +53,53 @@ daily_task_time = 0
 # Blynk notification values
 misting_counter = 0
 watering_needed = False
+
+
+def attempt_interface_creation(interface_name: str, function: callable) -> None:
+    """Makes multiple attempts to run a interface creating function, shuts down program if all fail"""
+    print(f"Attempting to create {interface_name} interface")
+
+    for attempt in range(MAX_INTERFACE_ATTEMPTS):
+        try:
+            function()
+            print("Success")
+            break
+        except Exception as e:
+            print(e)
+            if attempt == MAX_INTERFACE_ATTEMPTS - 1:
+                print("Max connections attempts exceeded, shutting down app")
+                exit()
+            else:
+                print("Attempting connection again")
+
+
+def create_hardware_interface() -> None:
+    """Creates interface for interacting with hardware"""
+    global hardware_manager
+    hardware_manager = HardwareManager()
+
+
+def create_blynk_interface() -> None:
+    """Creates interface for interacting with Blynk"""
+    global blynk
+    blynk = BlynkLib.Blynk(BLYNK_AUTH)
+    blynk.on("V4", blynk_daily_hour_change)
+
+
+def create_container_interface() -> None:
+    """Creates interface for interacting with an Azure blob storage container"""
+    global container
+    container = ContainerClient.from_connection_string(
+    AZURE_CONTAINER_AUTH,
+    AZURE_CONTAINER_NAME
+) 
+
+
+def create_custom_vision_interface() -> None:
+    """Creates interface for interacting with Custom Vision AI"""
+    global custom_vision
+    prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": CUSTOM_VISION_KEY})
+    custom_vision = CustomVisionPredictionClient(CUSTOM_VISION_ENDPOINT, prediction_credentials)
 
 
 def process_light_data() -> None:
@@ -103,12 +146,10 @@ def update_daily_task_time() -> None:
     print(f"Next time for daily tasks: {time_str}")
 
 
-@blynk.on("V4")
-#def handle_v4_write(value):
 def blynk_daily_hour_change(value):
     """Updates daily_task_hour based on user input through Blynk."""
     global daily_task_hour
-    daily_task_hour = value[0]
+    daily_task_hour = int(value[0])
     print(f'User set daily task hour to: {daily_task_hour}')
 
     update_daily_task_time()
@@ -129,14 +170,14 @@ def send_daily_notification() -> None:
         message += ", water the plant"
         
     blynk.log_event("daily_notification", message)
-    print("Sent daily notificaiton to Blynk with message:", message)
+    print("Sent daily notification to Blynk with message:", message)
     # TODO Add to dashboard daily tasks, for user clarity
 
 
 def upload_image() -> None:
     """Upload image from Pi camera to Azure Blob Storage"""
     with open(file="image.jpg", mode="rb") as data:
-        container_client.upload_blob(
+        container.upload_blob(
             name="image.jpg",
             data=data,
             overwrite=True,
@@ -148,7 +189,7 @@ def upload_image() -> None:
 def get_attacker_from_image() -> None:
     """Upload image to custom vision and return who was the attacker"""
     with open(file="image.jpg", mode="rb") as image:
-        results = custom_vision.classify_image(project_id, iteration_name, image)
+        results = custom_vision.classify_image(CUSTOM_VISION_PROJECT_ID, CUSTOM_VISION_ITERATION_NAME, image)
         print("Sent image to Custom Vision")
 
         print("Attacker predictions:")
@@ -161,8 +202,15 @@ def get_attacker_from_image() -> None:
         print("Sent attacker notification to Blynk with message:", message)
 
 
-# Main loop to process sensor data, send and receive data to/from Blynk, upload images to azure and manage timekeeping
+# Main loop to process sensor data, send and receive data to/from Blynk, upload images to azure and custom vision and manage timekeeping
 if __name__ == "__main__":
+    print("Attempting to create interfaces")
+    attempt_interface_creation("hardware", create_hardware_interface)
+    attempt_interface_creation("Blynk", create_blynk_interface)
+    attempt_interface_creation("Azure blob storage container", create_container_interface)
+    attempt_interface_creation("Custom Vision", create_custom_vision_interface)
+    print()
+
     try:
         while True:
             current_time = time.time()
